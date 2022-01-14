9d96ca2d26c2c21419f6429a801ba720a8d683a6
Alejandro Garcia <alejandrogg28@gmail.com>
2022-01-14 09:25:25 +0100

    operadores aritmeticos

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Construcción de expresións en PHP</title>
+</head>
+<body>
+    <h3>Construcción de expresións en PHP</h3>
+    <?php
+        //Operadores aritméticos
+        $x=10;
+        $y=3;
+        $z=$x*$y;
+        echo $x." x ".$y." = ".$z."<br/>";
+        $z=$x%$i;
+        echo $x." modulo ".$y." = ".$z."<br/>";
+    ?>
+</body>
+</html>
